f3a1779d8f9f0d0377ba2794f748a6ca89ea6917
roLLlyTHuK <ircvipua@gmail.com>
2023-12-26 19:31:51 +0200

    up

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,15 +1,29 @@
 import { Link, useLocation } from 'react-router-dom';
+import { INIT_POSTER_SIZE_500, INIT_PATH_IMAGE } from 'services/Api-request';
 
 const MoviesList = ({ movies }) => {
   const location = useLocation();
-
+  console.log('movies :>> ', movies);
   return (
-    <ul>
+    <ul
+      style={{
+        listStyle: 'none',
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '20px',
+        alignItems: 'center',
+      }}
+    >
       {movies &&
         movies.map(item => (
-          <li key={item.id}>
+          <li key={item.id} style={{ width: '250px' }}>
             <Link to={`/movies/${item.id}`} state={{ from: location }}>
-              {item.title}
+              {/* {item.title} */}
+              <img
+                src={`${INIT_PATH_IMAGE}${INIT_POSTER_SIZE_500}${item.poster_path}`}
+                width="250"
+                alt={item.title}
+              />
             </Link>
           </li>
         ))}
