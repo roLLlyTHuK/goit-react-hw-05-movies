63ad745e72309215750d609e0e8aee8430a723ad
roLLlyTHuK <ircvipua@gmail.com>
2024-01-16 14:46:27 +0200

    up

--- src/components/Cast/Cast.styled.js
+++ src/components/Cast/Cast.styled.js
@@ -16,7 +16,6 @@ export const List = styled.ul`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  gap: 10px;
   padding: 0;
 `;
 
@@ -30,7 +29,7 @@ export const Item = styled.li`
   border: 1px solid orange;
   border-radius: 16px;
   padding: 10px;
-  margin: 0 auto;
+  margin: 4px auto;
   width: 300px;
   box-shadow: 0 0 10px rgba(255, 165, 0, 0.5);
   transition: 0.3s ease-out;
